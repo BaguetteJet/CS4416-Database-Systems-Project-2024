d269749169f542a385778b26aed6026f7c1e54e2
CiaranW005 <ciaranwhelan245@gmail.com>
2024-11-13 15:57:00 +0000

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,15 +1,15 @@
 CREATE VIEW concert_stats AS
 SELECT a.artist_id,
        a.artist_name,
-       (SELECT SUM(DISTINCT s.length)
+       (SELECT ROUND(SUM(DISTINCT s.length), 2)
         FROM artist_to_song ats
         JOIN songs s ON ats.song_id = s.song_id
-        WHERE ats.artist_id = a.artist_id) AS time_played_for,
+        WHERE ats.artist_id = a.artist_id) AS total_time_played_for,
        
-       (SELECT SUM(DISTINCT ct.ticket_price)
+       (SELECT ROUND(SUM(DISTINCT ct.ticket_price), 2)
         FROM artist_to_concert atc
         JOIN concert_tickets ct ON atc.concert_id = ct.concert_id
-        WHERE atc.artist_id = a.artist_id) AS total_revenue
+        WHERE atc.artist_id = a.artist_id) AS total_revenue_made
 
 FROM artists a
 JOIN artist_to_concert atc ON a.artist_id = atc.artist_id
