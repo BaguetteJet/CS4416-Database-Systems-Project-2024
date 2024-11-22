dae905f80e941663898d41e2bf68db4ee72ce01c
CiaranW005 <ciaranwhelan245@gmail.com>
2024-11-22 20:44:47 +0000

    Update view.sql

--- other/view.sql
+++ other/view.sql
@@ -1,8 +1,8 @@
 CREATE VIEW concert_stats AS
 SELECT a.artist_id, -- Gets artistID
-       a.artist_name, -- Gets artists name
+       a.artist_name, -- Gets artist name
        
-       SUM( -- Sums all artists time at concerts together
+       SUM( -- Sums all artists' time at concerts together
               (SELECT ROUND(SUM(s.length), 2) -- Calculates the time an artist played for at a concert
                FROM songs s
                JOIN concert_to_songs cts ON s.song_id = cts.song_id -- Join used to find which songs have been played at a concert
@@ -21,5 +21,5 @@ SELECT a.artist_id, -- Gets artistID
 
 FROM artists a
 JOIN artist_to_concert atc ON a.artist_id = atc.artist_id -- Join artists to their concerts
-GROUP BY a.artist_id, a.artist_name -- Seperates the values by ther artist
-HAVING total_time_played_for > 7; -- Only includes artists that have significant play time
+GROUP BY a.artist_id, a.artist_name -- Separates the values by the artist
+HAVING total_time_played_for > 7; -- Only includes artists that have significant playtime
